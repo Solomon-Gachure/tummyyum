0810c5ee5ebd38543b8970ba573912c2d5ca52fc
Solomon Gachure <gachuresolomon64@gmail.com>
2023-09-27 13:06:36 +0300

    Added the food jsx file with responsive food cards with their prices using javascript

--- src/components/food.jsx
+++ src/components/food.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from "../data/data.js"
 const Food = () => {
+    const [foods, setFoods] = useState(data)
     return (
         <div className='max-w-[1640px] m-auto py-2 px-4 '>
             <h1 className='text-orange-600 font-bold text-center text-4xl'>Top Rated Menu</h1>
@@ -18,18 +19,31 @@ const Food = () => {
 
                     </div>
                 </div>
-                    {/*filter price */}
+                {/*filter price */}
+                <div>
+                    <p className='font-bold text-xl'>Filter Price</p>
                     <div>
-                        <p className='font-bold text-xl'>Filter Price</p>
-                        <div>
-                            <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
-                            <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
-                            <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
-                            <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
-                        </div>
-
+                        <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$</button>
+                        <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$$</button>
+                        <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$</button>
+                        <button className='m-1 border-orange-500 text-orange-600 hover:bg-orange-600 hover:text-white'>$$$$</button>
                     </div>
-                
+
+                </div>
+
+            </div>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+                {foods.map((items, index) => (
+                    <div key={index} className='border shadow-lg hover:scale-105 duration-300 rounded-lg'>
+                        <img src={items.image} alt={items.name} className='w-full h-[200px] object-cover rounded-t-lg' />
+                        <div className='px-2 py-2'>
+                            <p className='font-bold'>{items.name}</p>
+                            <p>
+                                <span className='font-bold text-green-500'>{items.price}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
